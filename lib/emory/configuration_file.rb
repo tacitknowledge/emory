f37b5b6edc49eb932e28f1d3c3fe06c4634f1c91
Ion Lenta <lentaion@gmail.com>
2012-05-04 15:34:22 +0300

    find .emory configuration file in current directory. if no file found, try to find it in parent directory and so on till reach root directory.

---
+++ lib/emory/configuration_file.rb
@@ -0,0 +1,43 @@
+module Emory
+
+  class ConfigurationFile
+    class << self
+      CONFIG_FILE_NAME = ".emory"
+      
+      def read
+        file = get_file Dir.pwd, CONFIG_FILE_NAME
+        if file.nil?
+          puts "configuration file NOT found"
+        else
+          puts "configuration file found: #{file}"
+        end
+      end
+      
+      private
+      
+      def get_file dir, file_name
+        puts "Start to search for configuration file: #{file_name}"
+        begin
+          puts "Searching for configuration file under: #{dir}"
+          file = get_file_full_path dir, file_name
+          break unless file.nil?
+          dir = get_parent_directory dir
+        end until dir == '/'
+        file
+      end
+      
+      def get_parent_directory dir
+        File.expand_path("..", dir)
+      end
+      
+      def file_exists? dir, file
+        File.exists?(File.expand_path(file, dir))
+      end
+      
+      def get_file_full_path dir, file
+          File.expand_path(file, dir) if file_exists?(dir, file)
+      end
+    end
+  end
+
+end
---
+++ test/emory/test_configuration_file.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+require 'emory/configuration_file'
+
+class TestConfigurationFile < Test::Unit::TestCase
+  
+  def test_read_file_not_found
+    #TODO: test come here
+  end
+  
+end
