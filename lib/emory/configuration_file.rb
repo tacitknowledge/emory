47abcb5601748690f8efad88f31d0167c44b3c7c
Ion Lenta <lentaion@gmail.com>
2012-05-10 22:40:48 +0300

    make it possible to locate .emory config file located in root directory ('/.emory')

--- lib/emory/configuration_file.rb
+++ lib/emory/configuration_file.rb
@@ -1,43 +1,49 @@
 module Emory
 
   class ConfigurationFile
+
+    CONFIG_FILE_NAME = ".emory"
+    START_SEARCH_INFO = "Start to search for configuration file: %s"
+    SEARCH_INFO = "Searching for configuration file under: %s"
+    FILE_FOUND_INFO = "configuration file found: %s"
+    FILE_NOT_FOUND_INFO = "configuration file NOT found"
+
     class << self
-      CONFIG_FILE_NAME = ".emory"
-      
-      def read
-        file = get_file Dir.pwd, CONFIG_FILE_NAME
-        if file.nil?
-          puts "configuration file NOT found"
+      def locate
+        puts START_SEARCH_INFO % CONFIG_FILE_NAME
+        file_full_path = locate_file Dir.pwd, CONFIG_FILE_NAME
+        if file_full_path.nil?
+          raise FILE_NOT_FOUND_INFO
         else
-          # TODO: raise error
-          puts "configuration file found: #{file}"
-          file
+          puts FILE_FOUND_INFO % file_full_path
+          file_full_path
         end
       end
       
       private
       
-      def get_file dir, file_name
-        puts "Start to search for configuration file: #{file_name}"
-        begin
-          puts "Searching for configuration file under: #{dir}"
-          file = get_file_full_path dir, file_name
-          break unless file.nil?
+      def locate_file dir, file_name
+        file_full_path = get_file_full_path dir, file_name
+        while file_full_path.nil?
           dir = get_parent_directory dir
-        end until dir == '/'
-        file
+          file_full_path = get_file_full_path dir, file_name
+          break if dir == '/'
+        end
+        file_full_path
       end
       
       def get_parent_directory dir
         File.expand_path("..", dir)
       end
       
-      def file_exists? dir, file
-        File.exists?(File.expand_path(file, dir))
-      end
-      
       def get_file_full_path dir, file
-        File.expand_path(file, dir) if file_exists?(dir, file)
+        puts SEARCH_INFO % dir
+        file_full_path = File.expand_path(file, dir)
+        if File.exists?(file_full_path)
+          file_full_path 
+        else
+          nil
+        end
       end
     end
   end
