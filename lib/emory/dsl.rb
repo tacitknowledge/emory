dcc96dcae505623a770db8b177474deac83ec250
Vladislav Gangan <vgangan@tacitknowledge.com>
2012-05-11 16:09:50 +0300

    First stab at the config DSL. For now only deals with handler configuration

---
+++ lib/emory/dsl.rb
@@ -0,0 +1,41 @@
+module Emory
+
+  class EmoryMisconfigurationException < Exception; end
+  class DuplicateHandlerNameException < Exception; end
+  class HandlerActionAllMustBeSingletonException < Exception; end
+  class UndefinedTeleportHandlerException < Exception; end
+
+  class Dsl
+    class << self
+      def instance_eval_emoryfile(contents, config_path)
+        config = new
+        config.instance_eval(contents, config_path, 1)
+        config
+      rescue Exception => e
+        puts "Incorrect contents of .emory file, original error is:\n#{ $! }"
+        raise EmoryMisconfigurationException, 'Incorrect contents of .emory file'
+      end
+    end
+
+    def handler(handler_name, class_name, options, *actions)
+      raise DuplicateHandlerNameException, "The handler name ':#{handler_name}' is defined more than once" if handlers.include?(handler_name)
+
+      handler = class_name.new(options)
+      uniq_actions = actions.uniq
+
+      raise HandlerActionAllMustBeSingletonException, "The handler action ':#{:all}' cannot be mixed with other types" if uniq_actions.include?(:all) and uniq_actions.size > 1
+      if uniq_actions[0] != :all
+        handler.instance_eval { undef :added } unless uniq_actions.include?(:added)
+        handler.instance_eval { undef :modified } unless uniq_actions.include?(:modified)
+        handler.instance_eval { undef :removed } unless uniq_actions.include?(:removed)
+      end
+
+      handlers[handler_name] = handler
+    end
+
+    def handlers
+      @handlers ||= {}
+    end
+  end
+
+end
---
+++ spec/emory/dsl_spec.rb
@@ -0,0 +1,84 @@
+require 'spec_helper'
+require 'emory/dsl'
+require 'emory/handlers/abstract_handler'
+
+module Emory
+
+  describe Dsl do
+    context "class object" do
+      it "parses the supplied handler config and configures correct object type" do
+        contents = <<-EOM
+          require 'emory/handlers/abstract_handler'
+          handler :something, Emory::Handlers::AbstractHandler, {}, :all
+        EOM
+
+        config = Dsl.instance_eval_emoryfile(contents, '/path/to/file')
+        config.handlers.size.should == 1
+        config.handlers[:something].class.should == Emory::Handlers::AbstractHandler
+      end
+
+      it "detects and reports incorrect usage of DSL" do
+        contents = <<-EOM
+          blah_blah_blah
+        EOM
+
+        Dsl.stub!(:puts)
+        Dsl.should_receive(:puts).with(/Incorrect contents of .emory file, original error is:\n/)
+
+        proc {
+          Dsl.instance_eval_emoryfile(contents, '/path/to/file')
+        }.should raise_error(EmoryMisconfigurationException,
+                             /Incorrect contents of .emory file/)
+      end
+    end
+
+    context "'handler' method" do
+      before(:each) do
+        @dsl = Dsl.new
+      end
+
+      it "passes the supplied options on to the handler's contructor" do
+        options = {host: 'localhost', port: '8080'}
+        Emory::Handlers::AbstractHandler.should_receive(:new).with(options)
+        @dsl.handler(:something, Emory::Handlers::AbstractHandler, options, :all)
+      end
+
+      it "does not allow to add multiple handlers with the same name" do
+        @dsl.handlers[:something] = Object.new
+        proc {
+          @dsl.handler(:something, nil, nil)
+        }.should raise_error(DuplicateHandlerNameException,
+                             /The handler name ':something' is defined more than once/)
+      end
+
+      it "mandates action of type ':all' to be exclusive of other types" do
+        proc {
+          @dsl.handler(:something, Emory::Handlers::AbstractHandler, {}, :added, :all)
+        }.should raise_error(HandlerActionAllMustBeSingletonException,
+                             /The handler action ':all' cannot be mixed with other types/)
+      end
+
+      it "with action of type ':all' does not undefine any handler's methods" do
+        @dsl.handler(:something, Emory::Handlers::AbstractHandler, {}, :all)
+        @dsl.handlers[:something].respond_to?(:added).should == true
+        @dsl.handlers[:something].respond_to?(:modified).should == true
+        @dsl.handlers[:something].respond_to?(:removed).should == true
+      end
+
+      it "with all action types does not undefine any handler's methods" do
+        @dsl.handler(:something, Emory::Handlers::AbstractHandler, {}, :added, :modified, :removed)
+        @dsl.handlers[:something].respond_to?(:added).should == true
+        @dsl.handlers[:something].respond_to?(:modified).should == true
+        @dsl.handlers[:something].respond_to?(:removed).should == true
+      end
+
+      it "undefines handler's methods which are not supplied" do
+        @dsl.handler(:something, Emory::Handlers::AbstractHandler, {}, :added)
+        @dsl.handlers[:something].respond_to?(:added).should == true
+        @dsl.handlers[:something].respond_to?(:modified).should == false
+        @dsl.handlers[:something].respond_to?(:removed).should == false
+      end
+    end
+  end
+
+end
