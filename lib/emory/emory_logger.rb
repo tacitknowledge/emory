09ba04f0de549418e15ab6455cdb83f04382f6e1
Ion Lenta <lentaion@gmail.com>
2012-05-14 12:23:05 +0300

    extract log file name to constant

--- lib/emory/emory_logger.rb
+++ lib/emory/emory_logger.rb
@@ -1,12 +1,14 @@
 require 'logging'
 
 module Emory
-
+  
+  LOG_FILE = 'emory.log'
+  
   class Logger
     
     Logging.logger.root.level = :info
     Logging.logger.root.add_appenders(
-      Logging.appenders.file('emory.log'), Logging.appenders.stdout)
+      Logging.appenders.file(LOG_FILE), Logging.appenders.stdout)
     
     class << self
   
