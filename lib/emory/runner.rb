30be2dbf3d467d2631e093ad1f8fc400aab151ae
Vladislav Gangan <vgangan@tacitknowledge.com>
2012-05-14 16:03:11 +0300

    Wiring in the configuration of Emory with  handling of events through Listen gem

--- lib/emory/runner.rb
+++ lib/emory/runner.rb
@@ -1,3 +1,4 @@
+require 'listen'
 require 'emory/configuration_file'
 require 'emory/dsl'
 
@@ -8,7 +9,33 @@ module Emory
       def start
         emory_config_file = ConfigurationFile.locate
         emory_config_contents = File.read(emory_config_file)
-        Emory::Dsl.instance_eval_emoryfile(emory_config_contents, emory_config_file)
+        config = Emory::Dsl.instance_eval_emoryfile(emory_config_contents, emory_config_file)
+
+        config.teleports.each do |teleport|
+          listener = Listen.to(teleport.watched_path)
+          listener.ignore(teleport.ignore) unless teleport.ignore.nil?
+          listener.filter(teleport.filter) unless teleport.filter.nil?
+          listener.change(&get_handler_callback(teleport.handler))
+          listener.start(false)
+        end
+
+        Thread.current.join
+      end
+
+      private
+
+      def get_handler_callback(handler)
+        Proc.new do |modified, added, removed|
+          trigger_handler_method(modified, handler, :modified)
+          trigger_handler_method(added, handler, :added)
+          trigger_handler_method(removed, handler, :removed)
+        end
+      end
+
+      def trigger_handler_method(paths, handler, operation)
+        unless paths.empty?
+          paths.each { |path| handler.send(operation, path) } if handler.respond_to?(operation)
+        end
       end
     end
   end
---
+++ spec/emory/runner_spec.rb
@@ -0,0 +1,41 @@
+require 'listen'
+require 'spec_helper'
+require 'emory/runner'
+require 'emory/dsl'
+require 'emory/teleport_config'
+require 'emory/handlers/stdout_handler'
+
+module Emory
+
+  describe Runner do
+    context "class object" do
+      it "starts the config file location process, creates necessary listeners and joins current thread" do
+        dsl = Dsl.new
+        teleport = TeleportConfig.new
+        teleport.watched_path = 'watched_path'
+        teleport.handler = Handlers::StdoutHandler.new
+        teleport.ignore = 'ignore_path'
+        teleport.filter = 'filter_expression'
+        dsl.teleports << teleport
+
+        path_to_config_file = '/path/to/config/file'
+        ConfigurationFile.stub(:locate => path_to_config_file)
+        File.stub(:read).with(path_to_config_file).and_return('contents')
+        Dsl.stub(:instance_eval_emoryfile).with('contents', path_to_config_file).and_return(dsl)
+
+        mock_listener = double("listener")
+        Listen.stub(:to).with(teleport.watched_path).and_return(mock_listener)
+        mock_listener.should_receive(:ignore).with(teleport.ignore)
+        mock_listener.should_receive(:filter).with(teleport.filter)
+        mock_listener.should_receive(:change)
+        mock_listener.should_receive(:start).with(false)
+
+        mock_thread = double("thread")
+        Thread.stub(:current => mock_thread)
+        mock_thread.should_receive(:join)
+
+        Runner.start
+      end
+    end
+  end
+end
