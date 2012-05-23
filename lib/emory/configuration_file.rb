058d6d7c280e150127d9f3fe5c45b632e244474a
Vladislav Gangan <vgangan@tacitknowledge.com>
2012-05-23 16:01:47 +0300

    Extracting the confg file name to constant

--- lib/emory/configuration_file.rb
+++ lib/emory/configuration_file.rb
@@ -7,12 +7,14 @@ module Emory
   class ConfigurationFile
 
     LOGGER = Logging.logger[self]
+    CONFIG_FILE_NAME = ".emory"
 
     class << self
+
       def locate
         Pathname.new(Dir.pwd).ascend do |dir|
           LOGGER.debug "Examining directory: #{dir}"
-          config_file = File.join(dir, ".emory")
+          config_file = File.join(dir, CONFIG_FILE_NAME)
           next unless File.exists?(config_file)
           LOGGER.info "Found config file: #{config_file}"
           return config_file
